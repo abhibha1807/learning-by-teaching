9afdaf854a88045d0c549e357f12ef15166c71d8
abhibha gupta <abhibha1807@gmail.com>
2022-02-09 16:07:50 +0530

    Delete DARTS directory

--- DARTS/train.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-nvidia-smi
-# conda env create -f environment.yml
-# conda update -n base -c defaults conda
-# source activate yxy
-# python arch_search.py
-# python train.py
-python train.py --arch 'arch_name'
